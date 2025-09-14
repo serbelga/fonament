292dd28171605eea960c8df22f68703ec150ef29
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2025-09-14 17:37:50 +0200

    Update FonamentViewModelOf with more generic types

--- fonament-di/koin/src/commonMain/kotlin/dev/sergiobelda/fonament/di/koin/FonamentViewModelOf.kt
+++ fonament-di/koin/src/commonMain/kotlin/dev/sergiobelda/fonament/di/koin/FonamentViewModelOf.kt
@@ -65,3 +65,88 @@ inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, rei
     crossinline constructor: (T1, T2, T3, T4, T5) -> FonamentViewModel<U>,
     noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
 ): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17, reified T18> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17, reified T18, reified T19> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17, reified T18, reified T19, reified T20> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17, reified T18, reified T19, reified T20, reified T21> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
+
+inline fun <reified U : FonamentUIState, reified T1, reified T2, reified T3, reified T4, reified T5, reified T6, reified T7, reified T8, reified T9, reified T10, reified T11, reified T12, reified T13, reified T14, reified T15, reified T16, reified T17, reified T18, reified T19, reified T20, reified T21, reified T22> Module.fonamentViewModelOf(
+    crossinline constructor: (T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22) -> FonamentViewModel<U>,
+    noinline options: (BeanDefinition<FonamentViewModel<U>>.() -> Unit)? = null,
+): KoinDefinition<FonamentViewModel<U>> = fonamentViewModel { new(constructor) }.onOptions(options)
