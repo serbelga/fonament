1b00683cc9d8e045f854fcc33fc77bd0acc83b6e
Sergio Belda Galbis <sbeldagalbis@gmail.com>
2026-02-01 17:13:20 +0100

    Update `deps` catalog to v2026.02.00

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("deps") {
-            from("dev.sergiobelda.projectconfig.catalog:deps:2026.01.03")
+            from("dev.sergiobelda.projectconfig.catalog:deps:2026.02.00")
         }
     }
 }
